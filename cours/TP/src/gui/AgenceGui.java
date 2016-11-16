a32c60c4193985385300ec679aa0d985ff2f222c
MaD <maximdegres@gmail.com>
2016-11-16 15:24:49 +0100

    change position vendeurs

--- cours/TP/src/gui/AgenceGui.java
+++ cours/TP/src/gui/AgenceGui.java
@@ -68,7 +68,7 @@ public class AgenceGui extends JFrame {
 		Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
 		int screenWidth = (int) screenSize.getWidth();
 		int width = this.getWidth();
-		int xx = (noAgenceGui * width) % screenWidth - this.getWidth();
+		int xx = (noAgenceGui * width - this.getWidth()) % screenWidth;
 		int yy = ((noAgenceGui * width) / screenWidth) * getHeight();
 		setLocation(xx, yy);
 		setTitle(myAgent.getLocalName());
